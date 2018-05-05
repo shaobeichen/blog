d0cb9aeeea8db6c9439e41bfaef00e694b082ca0
LeachZhou <leachzhou@163.com>
2018-05-05 20:48:06 +0800

    更新首页

--- hm.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?2b66959bf6bece96f67e9dbc69c38cda";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
