f5d62deb2bd6afcdd3ff3f176beccef06dc38a22
LeachZhou <leachzhou@163.com>
2018-05-11 17:36:37 +0800

    update token

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
     proxyTable: {
       '/api': {
         target: 'https://api.github.com/',
--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,11 @@
     name: 'App',
     data() {
       return {
-        token: '2b4752974261f38cdc0a' + '60af3bc2a4a138ae7c5c'
+        token: ['0f6560744b42121c3180d5','0684ff2aad37cc36fd']
       }
     },
     mounted() {
-      this.$axios.get("https://api.github.com/repos/LeachZhou/blog/issues", {
-        "access_token": this.token,
+      this.$axios.get(`api/repos/LeachZhou/blog/issues?access_token=${this.token[0]}${this.token[1]}`, {
         "page": 1,
         "per_page": 10,
         "filter": 'created'
