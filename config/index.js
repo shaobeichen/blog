e9f7c2d1152d3b1c8afb178fd74eaf31ea0e9184
周理强 <zhouliqiang@ibeifeng.com>
2018-05-28 17:59:31 +0800

    :sparkles: 修改打包环境

--- config/index.js
+++ config/index.js
@@ -47,10 +47,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../docs/index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../docs'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
 
