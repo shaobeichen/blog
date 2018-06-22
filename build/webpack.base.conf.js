15ac2b7ab0c1c5f1aaed103cc010e7048e8d30c5
leachzhou <leachzhou@163.com>
2018-06-22 17:29:17 +0800

    fix:添加自定义光标

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -58,7 +58,7 @@ module.exports = {
         }
       },
       {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        test: /\.(woff2?|eot|ttf|otf|cur)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
