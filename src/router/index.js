1a2ee65df6a32ddd2b1917c652f95861420d259f
leachzhou <leachzhou@163.com>
2018-06-25 10:01:25 +0800

    fix:添加路由scrollBehavior

--- src/router/index.js
+++ src/router/index.js
@@ -35,5 +35,12 @@ export default new Router({
       }
     }
 
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return {x: 0, y: 0}
+    }
+  }
 })
