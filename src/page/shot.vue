46f3f3a441f4b1ddf2b8ad442d81aaa63ffc341d
leachzhou <leachzhou@163.com>
2018-07-05 12:02:52 +0800

    feat:shot完成

--- src/page/shot.vue
+++ src/page/shot.vue
@@ -44,40 +44,27 @@
         document.onmousemove = function (e) {
           e.preventDefault();
           let distX = e.clientX - startX;//鼠标x轴移动了多少，正数往右，负数往左
-          Left.style.width = leftW + distX + 'px';
           Right.style.width = rightW - distX + 'px';
           Right.style.backgroundPositionX = rightBPX.split('px')[0] - distX + 'px';
           draggableBtn.style.left = parseInt(draggableBtnLeft.split('px')[0]) + distX + 'px';
-          let lrBor = innerW - draggableBtnW / 2;//边界 如960 - 58/2 = 931
-          let draggableBtn2W = draggableBtnW / 2;//边界 如58 / 2 = 29
-          if (parseInt(Left.style.width) >= lrBor) {
-            Left.style.width = lrBor + 'px';
-            // Right.style.width = rightW - lrBor + 'px';
-            // Right.style.backgroundPositionX = rightBPX.split('px')[0] - lrBor + 'px';
-            draggableBtn.style.left = draggableBtn2W + 'px';
-          }
-          if (parseInt(Left.style.width) <= draggableBtn2W) {
-            Left.style.width = draggableBtn2W + 'px';
-            Right.style.width = rightW - draggableBtn2W + 'px';
-            Right.style.backgroundPositionX = draggableBtn2W + 'px';
-            draggableBtn.style.left = lrBor + 'px';
-          }
+          let lrBor = innerW - draggableBtnW / 2;//边界 如960 - 60/2 = 930
+          let draggableBtn2W = draggableBtnW / 2;//边界 60 / 2 = 30
           if (parseInt(Right.style.width) >= lrBor) {
             Right.style.width = lrBor + 'px';
-            // Left.style.width = leftW - lrBor + 'px';
-            // Right.style.backgroundPositionX = rightBPX.split('px')[0] - lrBor + 'px';
+            Right.style.backgroundPositionX = '-' + draggableBtn2W + 'px';
             draggableBtn.style.left = draggableBtn2W + 'px';
           }
           if (parseInt(Right.style.width) <= draggableBtn2W) {
             Right.style.width = draggableBtn2W + 'px';
-            Left.style.width = leftW - draggableBtnW + 'px';
-            Right.style.backgroundPositionX = draggableBtn2W + 'px';
+            Right.style.backgroundPositionX = '-' + lrBor + 'px';
             draggableBtn.style.left = lrBor + 'px';
           }
         }
         document.onmouseup = function () {
           document.onmousemove = null;
+          document.onmouseup = null;
         }
+        return false;
       }
     },
     components: {
@@ -124,12 +111,12 @@
       left: 50%;
       transform: translate(-50%, 0);
       height: 100%;
-      width: 58px;
+      width: 60px;
       z-index: 3;
       text-align: center;
       margin: 0 auto;
       .in-moveline {
-        margin-left: 27px;
+        margin-left: 29px;
         width: 1px;
         height: 100%;
         background-color: #fff;
