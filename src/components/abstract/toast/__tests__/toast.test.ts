46e82fe7e742d7501a61d59718131429f111d084
shaobeichen <shaobeichen@outlook.com>
2022-07-06 13:49:45 +0800

    chore: update toast name

---
+++ src/components/abstract/toast/__tests__/toast.test.ts
@@ -0,0 +1,22 @@
+import { mount } from '@vue/test-utils'
+import { sleep } from '../../../../utils/index'
+
+import UIToast from '../Toast.vue'
+import Toast from '../Toast'
+
+test('创建一个弹窗，持续3秒', () => {
+  const wrapper = mount(UIToast, {
+    props: {
+      message: '提示成功',
+      duration: 3000
+    }
+  })
+  // expect(wrapper.find('.toast')).toBeTruthy()
+  expect(wrapper.text()).toContain('提示成功')
+})
+
+test('命令式调用，创建一个弹窗', async () => {
+  Toast('提示成功')
+  await sleep()
+  expect(document.body.querySelector('.toast')).toBeTruthy()
+})
