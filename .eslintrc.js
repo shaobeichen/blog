3c33db4c220f5947aa2d9dd2c576ab15bdfd369a
shaobeichen <shaobeichen@outlook.com>
2022-07-07 23:54:04 +0800

    fix: handle eslint stylelint prettier conflict

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,27 +3,27 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    // 使用 eslint-plugin-vue 解决 Vue3 defineProps、defineEmits、no-undef 规则警告
+    // https://eslint.vuejs.org/user-guide/#usage
     'vue/setup-compiler-macros': true
   },
   parser: 'vue-eslint-parser',
   extends: [
-    'plugin:vue/base',
-    'eslint:recommended',
-    'plugin:vue/vue3-recommended',
-    'plugin:vue/essential',
-    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'eslint-config-prettier'
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
     sourceType: 'module'
   },
-  plugins: ['vue', 'prettier', '@typescript-eslint'],
+  plugins: ['prettier', '@typescript-eslint'],
   rules: {
+    'prettier/prettier': 'error',
     'vue/no-v-html': 'off',
-    'vue/multi-word-component-names': 0,
+    'vue/multi-word-component-names': 'off',
     'vue/no-multiple-template-root': 'off',
     'vue/component-definition-name-casing': ['warn', 'kebab-case'],
     'no-debugger': 'off',
--- stylelint.config.js
+++ stylelint.config.js
@@ -1,32 +1,14 @@
 module.exports = {
-  extends: ['stylelint-config-standard-scss', 'stylelint-config-recommended-vue/scss'],
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {
     indentation: 2,
-    'font-family-name-quotes': 'always-unless-keyword',
-    'function-url-quotes': 'always',
-    'selector-attribute-quotes': 'always',
-    'string-quotes': 'single',
-    'color-function-notation': 'legacy',
-    'alpha-value-notation': 'number',
-    'at-rule-empty-line-before': [
-      'always',
-      { ignore: ['inside-block', 'blockless-after-same-name-blockless'] }
-    ],
-    'rule-empty-line-before': [
-      'always',
-      {
-        except: [
-          'after-rule',
-          'after-single-line-comment',
-          'first-nested',
-          'inside-block',
-          'inside-block-and-after-rule'
-        ],
-        ignore: ['after-comment', 'first-nested', 'inside-block']
-      }
-    ],
-    'property-no-vendor-prefix': [true, { ignoreProperties: ['background-clip'] }]
+    'unit-no-unknown': null,
+    'property-no-unknown': null,
+    'at-rule-no-unknown': null,
+    'no-descending-specificity': null,
+    'selector-pseudo-element-no-unknown': null,
+    'font-family-no-missing-generic-family-keyword': null
   }
 }
