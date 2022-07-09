7f59d437ea92f031c0e9780727da2739d8e881a3
shaobeichen <shaobeichen@outlook.com>
2022-07-10 00:39:00 +0800

    chore: set allow cross domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,40 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  plugins: [
-    vue(),
-    eslintPlugin({
-      include: ['src/**/*.ts', 'src/**/*.js', 'src/**/*.vue', 'src/*.ts', 'src/*.js', 'src/*.vue']
-    })
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/assets/style/variable.scss";`
+export default ({ mode }) => {
+  const { VITE_MOFISH_URL } = loadEnv(mode, process.cwd())
+
+  return defineConfig({
+    base: './',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    },
+    server: {
+      proxy: {
+        '/mofish': {
+          target: VITE_MOFISH_URL,
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/mofish/, '')
+        }
+      }
+    },
+    plugins: [
+      vue(),
+      eslintPlugin({
+        include: ['src/**/*.ts', 'src/**/*.js', 'src/**/*.vue', 'src/*.ts', 'src/*.js', 'src/*.vue']
+      })
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "./src/assets/style/variable.scss";`
+        }
       }
     }
-  }
-})
+  })
+}
