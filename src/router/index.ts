f7e40484bb51e988d4c22c0dff5ab1e973384cbc
shaobeichen <shaobeichen@outlook.com>
2022-07-10 00:43:16 +0800

    fix: fix can't backtop after router switch

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,15 +2,20 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 import index from '../pages/index.vue'
 import todolist from '../pages/todolist.vue'
+import mofish from '../pages/mofish.vue'
 
 const routes = [
   { path: '/', component: index },
-  { path: '/todolist', component: todolist }
+  { path: '/todolist', component: todolist },
+  { path: '/mofish', component: mofish }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
+  scrollBehavior() {
+    return { left: 0, top: 0 }
+  }
 })
 
 export default router
