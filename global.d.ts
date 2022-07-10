6844220bbc9efc32d27829d797a1ba48a05b71be
shaobeichen <shaobeichen@outlook.com>
2022-07-10 16:48:12 +0800

    chore: add ImportMetaEnv type define

--- global.d.ts
+++ global.d.ts
@@ -3,3 +3,10 @@ declare module '*.vue' {
   const Component: ReturnType<typeof defineComponent>
   export default Component
 }
+
+interface ImportMetaEnv {
+  VITE_BASE_URL: string
+  VITE_BASE_LOCAL_URL: string
+  VITE_MOFISH_URL: string
+  VITE_MOFISH_LOCAL_URL: string
+}
